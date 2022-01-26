bb5626a377c1ae90e72562fc0c7b6856a956c8e1
skienzmatishun <skienzmatishun@aol.com>
2022-01-25 23:36:11 -0600

    settings

--- src/website-config.ts
+++ src/website-config.ts
@@ -56,12 +56,12 @@ export interface WebsiteConfig {
 const config: WebsiteConfig = {
   title: 'Deal with the Devil',
   description: 'Wrongful Conviction',
-  coverImage: 'img/blog-cover.png',
+  coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  siteUrl: 'https://deal-with-the-devil.com',
+  facebook: 'https://www.facebook.com/therippreport',
+  twitter: 'https://twitter.com/',
   showSubscribe: true,
   mailchimpAction:
     'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
