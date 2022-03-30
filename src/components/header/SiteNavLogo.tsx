546e11cfe2779251b32636460ca42268e42aad02
skienzmatishun@aol.com <skienzmatishun@aol.com>
2022-03-30 16:48:12 -0500

    changed logo to svg/css

--- src/components/header/SiteNavLogo.tsx
+++ src/components/header/SiteNavLogo.tsx
@@ -1,34 +1,15 @@
-import { graphql, Link, StaticQuery } from 'gatsby';
+import { Link } from 'gatsby';
 import React from 'react';
 import { css } from '@emotion/react';
-import { getSrc } from 'gatsby-plugin-image';
 
-import config from '../../website-config';
 
-interface SiteNavLogoProps {
-  logo?: any;
-}
 
 export const SiteNavLogo = () => (
-  <StaticQuery
-    query={graphql`query HeadingQuery {
-  logo: file(relativePath: {eq: "img/ghost-logo.png"}) {
-    childImageSharp {
-      gatsbyImageData(quality: 100, width: 500, layout: FIXED)
-    }
-  }
-}
-`}
-    render={(data: SiteNavLogoProps) => (
+ 
       <Link className="site-nav-logo" css={SiteNavLogoStyles} to="/">
-        {data.logo ? (
-          <img src={getSrc(data.logo)} alt={config.title} />
-        ) : (
-          config.title
-        )}
+       <div className="deal-logo" />
       </Link>
-    )}
-  />
+ 
 );
 
 const SiteNavLogoStyles = css`
@@ -53,6 +34,14 @@ const SiteNavLogoStyles = css`
     display: block;
     width: auto;
     height: 21px;
+  }
+  .deal-logo{
+    background-image: url("data:image/svg+xml,%3Csvg width='118.682' height='87.958' viewBox='0 0 31.401 23.272' xmlns='http://www.w3.org/2000/svg'%3E%3Ctext xml:space='preserve' style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-size:10.5833px;line-height:1.25;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.264583' x='24.655' y='275.645' transform='translate(-23.473 -268.067)'%3E%3Ctspan style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.264583' x='24.655' y='275.645'%3EDEAL%3C/tspan%3E%3C/text%3E%3Cpath style='fill:%23fff;fill-opacity:.996078;stroke-width:9.5;stroke-opacity:.996078' transform='translate(.007 -141.258) scale(.20472)' d='m68.268 711.99.036-3.835.037-3.834 3.302 1.949 3.302 1.949-3.339 1.885z'/%3E%3Cpath style='fill:%23fff;fill-opacity:1;stroke-width:1.45188;stroke-opacity:.996078' d='M24.758 271.421h10.398v.806H24.758z' transform='translate(-23.473 -268.067)'/%3E%3Ctext xml:space='preserve' style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-size:5.99004px;line-height:1.25;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.149751' x='11.434' y='247.728' transform='translate(-11.542 -233.464)'%3E%3Ctspan style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.149751' x='11.434' y='247.728'%3EWITH THE%3C/tspan%3E%3C/text%3E%3Ctext xml:space='preserve' style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-size:10.5833px;line-height:1.25;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.264583' x='11.722' y='256.715' transform='translate(-11.542 -233.464)'%3E%3Ctspan style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.264583' x='11.722' y='256.715'%3EDEVIL%3C/tspan%3E%3C/text%3E%3C/svg%3E");
+background-repeat: no-repeat no-repeat;
+background-position: center center;
+background-size: cover;
+height:19px;
+width:26px;
   }
 `;
 
--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -89,11 +89,7 @@ const IndexPage: React.FC<IndexProps> = props => {
             <SiteHeaderContent className="site-header-content">
               <SiteTitle className="site-title">
                 {props.data.logo ? (
-                  <img
-                    style={{ maxHeight: '250px' }}
-                    src={getSrc(props.data.logo)}
-                    alt={config.title}
-                  />
+                 <div className="deal-logo" />
                 ) : (
                   config.title
                 )}
@@ -326,6 +322,15 @@ padding:.5rem;
   padding:1.5rem;
 margin:2rem;
 color:white;
+}
+
+.deal-logo {
+background-image: url("data:image/svg+xml,%3Csvg width='118.682' height='87.958' viewBox='0 0 31.401 23.272' xmlns='http://www.w3.org/2000/svg'%3E%3Ctext xml:space='preserve' style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-size:10.5833px;line-height:1.25;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.264583' x='24.655' y='275.645' transform='translate(-23.473 -268.067)'%3E%3Ctspan style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.264583' x='24.655' y='275.645'%3EDEAL%3C/tspan%3E%3C/text%3E%3Cpath style='fill:%23fff;fill-opacity:.996078;stroke-width:9.5;stroke-opacity:.996078' transform='translate(.007 -141.258) scale(.20472)' d='m68.268 711.99.036-3.835.037-3.834 3.302 1.949 3.302 1.949-3.339 1.885z'/%3E%3Cpath style='fill:%23fff;fill-opacity:1;stroke-width:1.45188;stroke-opacity:.996078' d='M24.758 271.421h10.398v.806H24.758z' transform='translate(-23.473 -268.067)'/%3E%3Ctext xml:space='preserve' style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-size:5.99004px;line-height:1.25;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.149751' x='11.434' y='247.728' transform='translate(-11.542 -233.464)'%3E%3Ctspan style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.149751' x='11.434' y='247.728'%3EWITH THE%3C/tspan%3E%3C/text%3E%3Ctext xml:space='preserve' style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-size:10.5833px;line-height:1.25;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.264583' x='11.722' y='256.715' transform='translate(-11.542 -233.464)'%3E%3Ctspan style='font-style:normal;font-variant:normal;font-weight:400;font-stretch:normal;font-family:Aleo;-inkscape-font-specification:Aleo;fill:%23fff;stroke-width:.264583' x='11.722' y='256.715'%3EDEVIL%3C/tspan%3E%3C/text%3E%3C/svg%3E");
+background-repeat: no-repeat no-repeat;
+background-position: center center;
+background-size: cover;
+height:200px;
+width:275px;
 }
 
 @media (max-width: 795px) {
