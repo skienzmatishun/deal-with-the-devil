faa3c4ec01eead58020974fdc1825e7a7eb61361
skienzmatishun@aol.com <skienzmatishun@aol.com>
2023-07-20 14:02:09 -0500

    removed preact plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml.name',
   },
   plugins: [
-    `gatsby-plugin-preact`,
       {
           resolve: `gatsby-plugin-disqus`,
           options: {
