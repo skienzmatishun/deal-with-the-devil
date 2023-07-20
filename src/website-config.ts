5e7caba8ae7a9b8911ed2ebf0b4f428be473d162
skienzmatishun@aol.com <skienzmatishun@aol.com>
2023-07-20 15:04:53 -0500

    website config fix

--- src/website-config.ts
+++ src/website-config.ts
@@ -54,21 +54,16 @@ export type WebsiteConfig = {
 };
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Deal with the Devil',
+  description: 'Wrongful Conviction',
   coverImage: 'img/blog-cover.png',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
+  siteUrl: 'https://deal-with-the-devil',
+  facebook: 'https://www.facebook.com/dealdealwiththedevilblog',
   twitter: 'https://twitter.com/tryghost',
   showSubscribe: true,
-  mailchimpAction:
-    'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
-  mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
-  mailchimpEmailFieldName: 'MERGE0',
-  googleSiteVerification: 'GoogleCode',
-  footer: 'is based on Gatsby Casper',
+  footer: '',
   showAllTags: true,
 };
 
