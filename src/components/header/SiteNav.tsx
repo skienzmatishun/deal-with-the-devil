f64370f357f6e2ea1e5e08f094179a03bcad88f5
unknown <skienzmatishun@aol.com>
2023-08-15 15:36:05 -0500

    idfk

--- src/components/header/SiteNav.tsx
+++ src/components/header/SiteNav.tsx
@@ -88,6 +88,7 @@ class SiteNav extends React.Component<SiteNavProps, SiteNavState> {
           <SiteNavLeft className="site-nav-left">
             {!isHome && <SiteNavLogo />}
             <SiteNavContent css={[this.state.showTitle ? HideNav : '']}>
+			        <p>Sponsor: <a href="https://rippreport.com">The Ripp Report</a></p>
               <ul css={NavStyles} role="menu">
                 <li style={{display:"block", padding: "12px 12px", color: "#fff", opacity: "0.8"}}>
                     Affiliated Pages:
@@ -122,7 +123,6 @@ class SiteNav extends React.Component<SiteNavProps, SiteNavState> {
           
         </nav>
         {isHome &&(
-        <p>Sponsor: <a href="https://rippreport.com">The Ripp Report</a></p>
         )}
       </>
     );
