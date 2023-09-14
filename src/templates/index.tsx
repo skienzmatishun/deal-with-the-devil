1c2b1ae4f73b82444eca3511babea7676d58eea9
unknown <skienzmatishun@aol.com>
2023-09-13 21:31:04 -0500

    button color for intro to red

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -129,7 +129,7 @@ const IndexPage: React.FC<IndexProps> = props => {
               <p>No timeline, two medical examiner reports that dispute state's cause of death.</p>
               <h2 style={{ marginTop: "20px" }} >Mission Statement </h2>
               <p>The release of wrongfully convicted Murray "Bubba" Lawrence Jr.</p>
-			  <a href="https://deal-with-the-devil.com/full-story/"> <span className="petition-button white" >Read the Full Story</span></a>
+			  <a href="https://deal-with-the-devil.com/full-story/"> <span className="petition-button red" >Read the Full Story</span></a>
             </div>
           </div>
         </div>
