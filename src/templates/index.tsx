2580fec6dc3ac22d8caf5e3eb71690fe8c56df09
unknown <skienzmatishun@aol.com>
2023-09-13 20:47:19 -0500

    removed topics added button to intro

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -129,10 +129,8 @@ const IndexPage: React.FC<IndexProps> = props => {
               <p>No timeline, two medical examiner reports that dispute state's cause of death.</p>
               <h2 style={{ marginTop: "20px" }} >Mission Statement </h2>
               <p>The release of wrongfully convicted Murray "Bubba" Lawrence Jr.</p>
+			  <a href="https://deal-with-the-devil.com/full-story/"> <span className="petition-button white" >Read the Full Story</span></a>
             </div>
-          </div>
-          <div className="topics">
-<a href="https://deal-with-the-devil.com/full-story/"> <span className="petition-button white" >Read the Full Story</span></a>
           </div>
         </div>
 
